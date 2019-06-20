75c7517258fe668f8b55dc00440f23f2be806381
Arvinxl <49877240+Arvinxl@users.noreply.github.com>
2019-06-20 19:22:20 +0800

    Update GitDemo.java

--- src/main/java/com/ithei/git/GitDemo.java
+++ src/main/java/com/ithei/git/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("这是初始化git项目");
         System.out.println("测试修改");
+        System.out.println("在平台上修改");
     }
 }
